0dcf7b700ffe19b02f403be1c4b4291bc1e6dbdf
JoltLaw <94019623+JoltLaw@users.noreply.github.com>
2022-06-15 10:39:29 -0600

    Mocha and Chai initial commits

---
+++ Checkout Code Kada/test/index.js
@@ -0,0 +1,38 @@
+const Checkout = require('../checkout.js');
+
+var expect = require('chai').expect;
+var checkout;
+
+beforeEach(function(){
+    checkout = new Checkout();
+    checkout.addItemPrice('a', 1);
+    checkout.addItemPrice('b', 2);
+})
+
+it('can calcutlate the current total', function(){
+    checkout.addItem('a');
+    expect(checkout.calculateTotal()).to.equal(1);
+
+})
+
+it('can add multiple items and get correct total', function(){
+checkout.addItem('a');
+checkout.addItem('b');
+expect(checkout.calculateTotal()).to.equal(3)
+});
+
+it('can add discount rule', function(){
+    checkout.addDiscount('a', 3, 2);
+})
+
+it('can apply discount rules to total', function(){
+    checkout.addDiscount('a', 3, 2);
+    checkout.addItem('a');
+    checkout.addItem('a');
+    checkout.addItem('a');
+    expect(checkout.calculateTotal()).to.equal(2);
+});
+
+it('Thros when item added with no price', function(){
+    expect(function(){checkout.addItem('c')}).to.throw();
+})
