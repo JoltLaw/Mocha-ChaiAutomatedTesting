5782fb89f08f9ad758cfdf546e7a3ae2cc590f42
JoltLaw <94019623+JoltLaw@users.noreply.github.com>
2022-06-15 10:43:15 -0600

    Delete getUsers.js

--- getUsers.js
+++
@@ -1,8 +0,0 @@
-
-var request = require('request');
-
-module.exports = function getUsers(callback){
-    request.get('https://www.mysite.com/api/users', function(err, res){
-        callback(JSON.parse(res.body));
-    });
-}
